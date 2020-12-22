2c29416256560e9f3f4f170bdb9d2cbae05db645
bdQ5xBvJhHUlofLidX <6XmwxtgNRYrQgBJusv51Soo9KjuCrVcHhlyec10ufKQAq0gzR3UILEgMOp9dxSAJC0im7Im9>
2020-12-22 15:15:48 +0100

    [TESTING]

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const playwright = require('playwright');
         async function waitUntilLoggedIn() {
             await openWebsite("https://web.whatsapp.com/")
             await console.log("Waiting to Scan QR-Code...");
-            await currentPage.waitForSelector("div._15OLJ");
+            await currentPage.waitForSelector("div._15OLJ", {timeout: 999999});
             await console.log("Logged in.");
             await currentPage.click("[title='Anime / Otaku (VG#0001)']");
             await console.log("Selected Group.")
@@ -45,11 +45,11 @@ const playwright = require('playwright');
 
                     //onLeave Event
                     if (messageContent.includes("hat die Gruppe verlassen")) {
-
+                        await onLeave();
                     }
                 }
                 await clearMessages();
-                await wait(8000);
+                await wait(10000);
             }
         }
 
@@ -61,35 +61,46 @@ const playwright = require('playwright');
 
         async function onJoin(messageContent, phoneNumber) {
 
-            if (phoneNumber.includes("1525")) {
+            if (phoneNumber.includes("+1")) {
                 await kickUser(phoneNumber);
                 await writeMessage("Bot erkannt! Er wurde automatisch removed. (╯°□°）╯");
                 return;
             }
 
-            await writeMessage("Herzlichen Willkommen in der Otaku-Gruppe :)!\n\n" +
-                "Bitte stelle dich uns erst einmal mit deinem Namen, Alter und Lieblingsanime vor. " +
-                "Du kannst bei Bedarf auch noch ein Bild von dir hinzufügen, das ist aber keine Pflicht! \n\n" +
-                "Zu dem vermeide bitte unnötigen Smalltalk hier. Der kann in der Hauptgruppe geführt werden:).\n\n" +
-                "Nachdem du dich vorgestellt hast, erhältst du einen Einladungslink in die Hauptgruppe. Viel Spaß bei uns:)!")
+            // await writeMessage("Herzlichen Willkommen in der Otaku-Gruppe :)!\n\n" +
+            //     "Bitte stelle dich uns erst einmal mit deinem Namen, Alter und Lieblingsanime vor. " +
+            //     "Du kannst bei Bedarf auch noch ein Bild von dir hinzufügen, das ist aber keine Pflicht! \n\n" +
+            //     "Zu dem vermeide bitte unnötigen Smalltalk hier. Der kann in der Hauptgruppe geführt werden:).\n\n" +
+            //     "Nachdem du dich vorgestellt hast, erhältst du einen Einladungslink in die Hauptgruppe. Viel Spaß bei uns:)!")
+            await writeMessage("User joined");
+        }
+
+        async function onLeave() {
+            await writeMessage("User Left");
         }
 
         async function kickUser(phoneNumber) {
             await openUserOptions();
-            await currentPage.click("[title='"+phoneNumber+"']")
-            await currentPage.click("text='Entfernen'");
-            await currentPage.click("[data-testid='x']");
+            await currentPage.click("[title='"+phoneNumber+"']") /* Click on the User with the phonenumber */
+            await currentPage.click("text='Entfernen'"); /* Press 'Entfernen' Button */
+            await currentPage.click("[data-testid='x']"); /* Press 'X' Button from User List */
+            await currentPage.click("[data-testid='x']"); /* Press 'X' Button from Group Settings */
         }
 
         async function openUserOptions() {
-            await currentPage.click("div#main >> [data-testid='default-group']");
-            await currentPage.click("span._1TBWy >> [data-testid='search']");
+            await currentPage.click("div#main >> [data-testid='default-group']"); /* Click on the 3 dots to open Group Settings*/
+            await currentPage.click("span._1TBWy >> [data-testid='search']"); /* Click on the Search Icon do open User List */
         }
 
         async function clearMessages() {
-            await currentPage.click("div#main >> [data-testid='menu']");
-            await currentPage.click("text='Nachrichten leeren'");
-            await currentPage.click("text='Leeren'");
+            await currentPage.waitForSelector("div#main >> [data-testid='menu']", {visible: true});
+            await currentPage.click("div#main >> [data-testid='menu']"); /* Click on the settings button */
+
+            await currentPage.waitForSelector("text='Nachrichten leeren'", {visible: true});
+            await currentPage.click("text='Nachrichten leeren'"); /* Open the Clear message dialog */
+
+            await currentPage.waitForSelector("text='Leeren'", {visible: true});
+            await currentPage.click("text='Leeren'"); /* Clear messages */
         }
 
         async function writeMessage(message) {
